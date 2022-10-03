1d7d54aceaf224f17ba0128472d8586bd7dd4a82
jpscorp21 <jpscorp21@gmail.com>
2022-10-03 13:04:37 -0300

    Agregando ruc a modelo persona

--- src/TiendaProductoNatural.Application/Models/PersonaCreateDto.cs
+++ src/TiendaProductoNatural.Application/Models/PersonaCreateDto.cs
@@ -4,6 +4,7 @@ public class PersonaCreateDto
 {
     public int Id { get; set; }
     public string Denominacion { get; set; } = string.Empty;
+    public string? Ruc { get; set; }
     public string? Telefono { get; set; }
     public string? Nombre { get; set; }
     public string? Apellido { get; set; }
